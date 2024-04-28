b57725a506b91d572d42cbabd7d3465646d6c69c
Jamee <codingjamee@gmail.com>
2024-04-28 17:20:50 +0900

    Feat:게시판 리스트 가져오기

---
+++ vue-practice/board/src/components/BoardLists.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="board-head">게시판</div>
+    <table>
+      <thead>
+        <tr>
+          <th>번호</th>
+          <th>제목</th>
+          <th>등록일</th>
+        </tr>
+      </thead>
+      <tbody v-for="(board, index) in boards" v-bind:key="board.id">
+        <router-link v-bind:to="`/board/${board.id}`">
+          <th>{{ index }}</th>
+          <th>{{ board.title }}</th>
+          <th>{{ board.create_at }}</th>
+        </router-link>
+      </tbody>
+    </table>
+    <hr />
+    <ul>
+      <li v-for="board in boards" v-bind:key="board.id"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getData } from "@/api";
+export default {
+  data() {
+    return {
+      boards: [],
+    };
+  },
+  created() {
+    getData("boards/").then((response) => (this.boards = response.data));
+  },
+};
+</script>
+
+<style scoped></style>
