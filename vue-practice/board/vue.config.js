78183dff6c039bcf77d1cda53a22c475d53b63f5
Jamee <codingjamee@gmail.com>
2024-04-28 16:54:38 +0900

    Feat: proxy 설정

--- vue-practice/board/vue.config.js
+++ vue-practice/board/vue.config.js
@@ -1,4 +1,34 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin("define").tap((definitions) => {
+      const existingDefinitions = definitions[0]["process.env"] || {};
+
+      const featureFlags = {
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      };
+
+      definitions[0]["process.env"] = Object.assign(
+        {},
+        existingDefinitions,
+        featureFlags
+      );
+
+      return definitions;
+    });
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    },
+  },
+});
