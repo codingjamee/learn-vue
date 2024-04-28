86219f9f57558f0ee8fda21584d3bfb9dc83def5
Jamee <codingjamee@gmail.com>
2024-04-28 19:50:13 +0900

    Design: router link위치 변경

--- vue-practice/board/src/components/BoardDetail.vue
+++ vue-practice/board/src/components/BoardDetail.vue
@@ -2,11 +2,11 @@
   <div>board detail</div>
   <div>{{ boardDetail }}</div>
   <button>수정하기</button>
-  <button>삭제하기</button>
+  <button @click="deleteBoard">삭제하기</button>
 </template>
 
 <script>
-import { getData } from "@/api";
+import { deleteData, getData } from "@/api";
 export default {
   data() {
     return {
@@ -14,9 +14,25 @@ export default {
     };
   },
   created() {
-    getData(`boards/${this.$route.params.id}/`).then(
-      (response) => (this.boardDetail = response.data)
-    );
+    getData(`boards/${this.$route.params.id}/`)
+      .then((response) => (this.boardDetail = response.data))
+      .catch((error) => {
+        console.error("Failed to fetch board details:", error);
+        this.$router.push("/board");
+      });
+  },
+  methods: {
+    deleteBoard() {
+      this.$router.push("/board");
+      deleteData(`boards/${this.$route.params.id}/`)
+        .then((response) => {
+          console.log(response);
+          this.$router.push("/board");
+        })
+        .catch((error) => {
+          console.error("Failed to delete board details:", error);
+        });
+    },
   },
 };
 </script>
