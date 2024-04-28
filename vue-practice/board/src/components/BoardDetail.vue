8752b41930a075c3cfea24539179f60a58aeabd4
Jamee <codingjamee@gmail.com>
2024-04-28 17:35:31 +0900

    Feat: 게시판 상세정보 가져오기

---
+++ vue-practice/board/src/components/BoardDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>board detail</div>
+  <div>{{ boardDetail }}</div>
+  <button>수정하기</button>
+  <button>삭제하기</button>
+</template>
+
+<script>
+import { getData } from "@/api";
+export default {
+  data() {
+    return {
+      boardDetail: {},
+    };
+  },
+  created() {
+    getData(`boards/${this.$route.params.id}/`).then(
+      (response) => (this.boardDetail = response.data)
+    );
+  },
+};
+</script>
+
+<style scoped></style>
