7ba34e10209acd7a28a71b7f42c4c4840e87daae
Jamee <codingjamee@gmail.com>
2024-04-28 21:52:58 +0900

    Feat: 수정하기 기능

--- vue-practice/board/src/api/index.js
+++ vue-practice/board/src/api/index.js
@@ -7,3 +7,7 @@ export const getData = (url) => {
 export const deleteData = (id) => {
   return axios.delete(`/api/${id}`);
 };
+
+export const putData = ({ id, data }) => {
+  return axios.put(`/api/${id}`, { ...data });
+};
